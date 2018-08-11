e9686287662e598a444f0912602a903147e3c728
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-08-12 02:24:04 +0900

    Add topbar template

--- meant/src/component/js/Template.js
+++ meant/src/component/js/Template.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import '../scss/css/template.css';
 
-const Template = ({topbar}) => {
+const Template = ({topbar, sidebar}) => {
     return (
         <div className="template">
             {topbar}
+            {sidebar}
         </div>
     );
 }
