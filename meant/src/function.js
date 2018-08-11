4a6d8efc3f026f158a946392c45f3038249ff010
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-08-12 02:29:40 +0900

    Functions component

---
+++ meant/src/function.js
@@ -0,0 +1,13 @@
+const _GotoMain = () => {
+    window.location.href = "http://localhost:3000";
+}
+
+const _GotoLogin = () => {
+    window.location.href = "http://localhost:3000/login"
+}
+
+const _GotoLookup = () => {
+    window.location.href = "http://localhost:3000/lookup"
+}
+
+export { _GotoMain, _GotoLogin, _GotoLookup }
