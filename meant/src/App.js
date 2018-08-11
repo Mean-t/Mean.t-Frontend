de16dfceb97ddcec512aab82977c42d120b8c098
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-08-11 14:29:21 +0900

    Add topbar component with any component

--- meant/src/App.js
+++ meant/src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import Template from './component/js/Template';
+import Topbar from './component/js/topbar/Topbar';
+import Menu from './component/js/topbar/nav/Menu';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Sample
+      <div className="Meant-Main">
+        <Template 
+          topbar = {<Topbar
+            navigation = {<Menu
+            />}
+          />}
+        />
       </div>
     );
   }
