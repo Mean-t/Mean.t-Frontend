3b9844494bb527c93cebddd771f45007dd5cc4db
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-08-13 03:32:58 +0900

    Add submain component

--- meant/src/App.js
+++ meant/src/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import Template from './component/js/Template';
 import Topbar from './component/js/topbar/Topbar';
 import Menu from './component/js/nav/Menu';
-import { _GotoMain, _GotoLogin, _GotoLookup } from './function';
+import Submain from './component/js/submain/SubMain';
+import Sendidea from './component/js/submain/send/SendIdea';
+import { _GotoMain, _GotoLogin, _GotoLookup, _SendImage } from './function';
 import './App.css';
 
 class App extends Component {
@@ -12,7 +14,6 @@ class App extends Component {
   }
 
   render() {
-
     const _OpenSideBar = () => {
       if (this.state.MenuState === "menu-unshow") {
         this.setState({
@@ -36,6 +37,11 @@ class App extends Component {
           />}
           sidebar = {<Menu 
             menu_state = {this.state.MenuState}
+          />}
+          submain = {<Submain
+            sendIdea = {<Sendidea 
+              SendIdea = {_SendImage}
+            />}
           />}
         />
       </div>
