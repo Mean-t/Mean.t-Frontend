f707888a66fe4d3049bfe4b857743ec35ebea7cc
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-08-13 03:33:20 +0900

    Add submain component

--- meant/src/component/js/Template.js
+++ meant/src/component/js/Template.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../scss/css/template.css';
 
-const Template = ({topbar, sidebar}) => {
+const Template = ({topbar, sidebar, submain}) => {
     return (
         <div className="template">
             {topbar}
             {sidebar}
+            {submain}
         </div>
     );
 }
