7d646fd06bfd38dde1c9770b50ae5a79fca9e4ca
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-08-14 01:01:54 +0900

    Create idealist component

---
+++ meant/src/component/js/fotter/list/Idealist.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../../scss/css/idealist.css';
+
+const IdeaList = () => {
+    return (
+        <div className="idea-list">
+            
+        </div>
+    );
+}
+
+export default IdeaList;
