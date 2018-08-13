74d6e0fa4598a65a8d82acb62877d1f6ea806b36
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-08-14 01:00:13 +0900

    Add Goto Functions

--- meant/src/function.js
+++ meant/src/function.js
@@ -3,15 +3,23 @@ const _GotoMain = () => {
 }
 
 const _GotoLogin = () => {
-    window.location.href = "http://localhost:3000/login"
+    window.location.href = "http://localhost:3000/login";
 }
 
 const _GotoLookup = () => {
-    window.location.href = "http://localhost:3000/lookup"
+    window.location.href = "http://localhost:3000/lookup";
 }
 
 const _SendImage = () => {
-    window.location.href = "http://localhost:3000/idea-send"
+    window.location.href = "http://localhost:3000/idea-send";
 }
 
-export { _GotoMain, _GotoLogin, _GotoLookup, _SendImage }
+const _GotoList = () => {
+    window.location.href = "http://localhost:3000/dress-list";
+}
+
+const _GotoIList = () => {
+    window.location.href = "http://localhost:3000/idea-list";
+}
+
+export { _GotoMain, _GotoLogin, _GotoLookup, _SendImage, _GotoList, _GotoIList }
