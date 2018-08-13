0a025b3e378d4ba745c13725fdb46a1d4f75defb
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-08-14 01:09:02 +0900

    Clothes list Component

---
+++ meant/src/component/js/main/list/DressList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../../scss/css/dresslist.css';
+
+const DressList = () => {
+    return (
+        <div className="fund-list">
+            
+        </div>
+    );
+}
+
+export default DressList;
