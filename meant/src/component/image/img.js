e609b3658e498ef5a635b63a892984f8402829ab
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-08-14 22:36:47 +0900

    Image component

---
+++ meant/src/component/image/img.js
@@ -0,0 +1,7 @@
+import airplane from './airplane.png';
+import leftArrow from './left-arrow.PNG';
+import menu from './menu.png';
+import rightArrow from './right-arrow.PNG';
+import Logo from './Logo.png';
+
+export {airplane, leftArrow, menu, rightArrow, Logo};
