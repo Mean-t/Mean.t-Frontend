82060b75a8d39afbc930dc6ef2dae816f3e3b109
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-08-15 14:57:41 +0900

    Change code style

--- meant/src/function.js
+++ meant/src/function.js
@@ -1,25 +1,32 @@
 const _GotoMain = () => {
-    window.location.href = "http://localhost:3000";
-}
+  window.location.href = "http://localhost:3000";
+};
 
 const _GotoLogin = () => {
-    window.location.href = "http://localhost:3000/login";
-}
+  window.location.href = "http://localhost:3000/login";
+};
 
 const _GotoLookup = () => {
-    window.location.href = "http://localhost:3000/lookup";
-}
+  window.location.href = "http://localhost:3000/lookup";
+};
 
 const _SendImage = () => {
-    window.location.href = "http://localhost:3000/idea-send";
-}
+  window.location.href = "http://localhost:3000/idea-send";
+};
 
 const _GotoList = () => {
-    window.location.href = "http://localhost:3000/dress-list";
-}
+  window.location.href = "http://localhost:3000/dress-list";
+};
 
 const _GotoIList = () => {
-    window.location.href = "http://localhost:3000/idea-list";
-}
+  window.location.href = "http://localhost:3000/idea-list";
+};
 
-export { _GotoMain, _GotoLogin, _GotoLookup, _SendImage, _GotoList, _GotoIList }
+export {
+  _GotoMain,
+  _GotoLogin,
+  _GotoLookup,
+  _SendImage,
+  _GotoList,
+  _GotoIList
+};
