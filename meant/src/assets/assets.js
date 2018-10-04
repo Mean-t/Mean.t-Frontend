b8a82998a9667235941f0a2ce5576135d20ab31c
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-10-04 19:41:05 +0900

    ADD : assete file

--- meant/src/assets/assets.js
+++ meant/src/assets/assets.js
@@ -3,12 +3,16 @@ import menuButton from './menuButton.JPG';
 import sendAirplan from './sendAirplan.png';
 import leftButton from './leftButton.JPG';
 import rightButton from './rightButton.JPG';
+import loginMessage from './loginMessage.JPG';
+import registerMessage from './registerMessage.JPG';
 
 export {
   Logo,
   menuButton,
   sendAirplan,
   leftButton,
-  rightButton
+  rightButton,
+  loginMessage,
+  registerMessage
   // 더 있으면 추가
 };
