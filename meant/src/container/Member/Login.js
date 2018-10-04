b7d799a39762c8bec2477d755a6270ae3216fe57
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-10-04 19:43:38 +0900

    ADD : Login Container

---
+++ meant/src/container/Member/Login.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+import { LoginComponent } from '../../components/Member/index';
+
+const Login = () => {
+  return(
+    <Container>
+      <LoginComponent>
+        
+      </LoginComponent>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  width : 100%:
+  height : 1244px;
+  background-color : #E9E9E9;
+  z-index : -1;
+  display : flex;
+  justify-content: center;
+  box-shadow: 0px -1px 22px #aaaaaa;
+`;
+
+export default Login;
