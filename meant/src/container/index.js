0e50b73d9854f053c4d07c16340219be39178820
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-10-07 11:58:55 +0900

    ADD : container

--- meant/src/container/index.js
+++ meant/src/container/index.js
@@ -2,11 +2,13 @@ import Main from './Main/Main';
 import Login from './Member/Login';
 import Register from './Member/Register';
 import CreateFunding from './CreateFunding/CreateFunding';
+import StartFunding from './StartFunding/StartFunding';
 
 export {
   Main,
   Login,
   Register,
-  CreateFunding
+  CreateFunding,
+  StartFunding
   // 더 있으면 추가
 }
