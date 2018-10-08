bbcc25d24544097cdd1f66e612168f9e8ed532ad
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-10-08 23:29:41 +0900

    주문번호조회 컨포넌트

---
+++ meant/src/container/OrderNumber/OrderNumber.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import Select from '../../components/OrderNumber/Select';
+
+class OrderNumber extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return ( 
+      <Container>
+        <Select />
+      </Container>
+     );
+  }
+}
+ 
+const Container = styled.div`
+  width : 100%;
+  height : 964px;
+  display : flex;
+  justify-content: center;
+  align-items : center;
+`;
+
+export default OrderNumber;
