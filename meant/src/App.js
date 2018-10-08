2ba44b73ad8d77fb62b6d5a5574f71b4cfebee1a
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-10-08 23:28:52 +0900

    ADD : Route

--- meant/src/App.js
+++ meant/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { Main, Login, Register, CreateFunding, StartFunding } from './container/index';
+import { Main, Login, Register, CreateFunding, StartFunding, OrderNumber } from './container/index';
 import NavigationBar from './components/default layout/NavigationBar';
 
 class App extends Component {
@@ -16,6 +16,7 @@ class App extends Component {
                 <Route path='/member/join_check' component={Register} exact/>
                 <Route path='/funding/create_idea' component={CreateFunding} exact/>
                 <Route path='/funding/start' component={StartFunding} exact/>
+                <Route path='/OrderNumber/reference' component={OrderNumber} exact/>
               </Switch>
           </React.Fragment>
         </BrowserRouter>
