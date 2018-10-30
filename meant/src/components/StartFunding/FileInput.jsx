fd9ee7b8202b4779f048694ddff4bca70d397210
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-10-30 14:58:12 +0900

    Add Props( mode coverFile handle buttonFor inputRef inputId width)

--- meant/src/components/StartFunding/FileInput.jsx
+++ meant/src/components/StartFunding/FileInput.jsx
@@ -3,29 +3,29 @@ import styled from 'styled-components';
 
 class FileInput extends Component {
   render() {
+
+    const { mode, coverFile, handle, buttonFor, inputRef, inputId, width } = this.props;
+
     return(
       <React.Fragment>
-        <SendButton cover for="cover_file">{this.props.coverFile === null ? this.props.mode : '✓ 추가 성공'}</SendButton>
-        <InputFile cover_btn onClick={e => e.target.value = null} onChange={this.props.handle} accept='image/*' type='file' ref='cover_file' id='cover_file'></InputFile>
+        <SendButton style={{width : width}} cover for={buttonFor}>{coverFile === null ? mode : '✓ 추가 성공'}</SendButton>
+        <InputFile cover_btn onClick={e => e.target.value = null} onChange={handle} accept='image/*' type='file' ref={inputRef} id={inputId}></InputFile>
       </React.Fragment>
     );
   }
 }
 
-export default FileInput;
-
-
 const SendButton = styled.label`
   display: inline-flex;
   color: #40CD9F; 
-  font-size: 22px; 
+  font-size: 25px; 
   background-color: #FFFFFF;
   cursor: pointer; 
   border: 3px solid #40CD9F; 
   border-radius : 60px;
-  width : 100%;
+  width : 30%;
   height : 70px;
-  margin-bottom : 40px;
+  /* margin-bottom : 40px; */
   justify-content : center;
   align-items : center;
   transition : 0.3s;
@@ -57,3 +57,5 @@ const InputFile = styled.input`
     }}
   }
 `;
+
+export default FileInput;
