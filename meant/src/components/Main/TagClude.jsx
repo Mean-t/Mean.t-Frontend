c9bcb1e3ab4f1fd7d07778f299ce741a613ecb81
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-11-02 15:40:13 +0900

    실험

--- meant/src/components/Main/TagClude.jsx
+++ meant/src/components/Main/TagClude.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import TagCloud from 'react-tag-cloud';
+import styled from 'styled-components'
 import Axios from 'axios';
 
 class TagClude extends Component {
@@ -23,16 +24,33 @@ class TagClude extends Component {
       <TagCloud
         style={{
           fontFamily : 'NanumSquareRound',
-          fontSize : 16,
+          fontSize : 42,
           color : '#26B0A7',
-          padding : 16,
+          padding : 12,
           width : '100%',
           height : '100%',
           
         }}>
-        <div>react</div>
-        <div>tag</div>
-        <div>cloud</div>
+        <div style={{fontSize : 42}}>1asdfasdfasdfsad</div>
+        <div style={{fontSize : 22}}>2fasdfsdfasd</div>
+        <div style={{fontSize : 22}}>3fasdfasdfasd</div>
+        <div style={{fontSize : 42}}>fasdfasdfasd4</div>
+        <div style={{fontSize : 22}}>fasdfasdfas5</div>
+        <div style={{fontSize : 22}}>dfasdfasdf6</div>
+        <div style={{fontSize : 42}}>7asdfasdfasd</div>
+        <div style={{fontSize : 22}}>8fasdfasd</div>
+        <div style={{fontSize : 22}}>9fasdfasdfasd</div>
+        <div style={{fontSize : 42}}>10fasdfasdfasd</div>
+        <div style={{fontSize : 22}}>11fasdfasdf</div>
+        <div style={{fontSize : 22}}>1asdfasdfasd2</div>
+        <div style={{fontSize : 42}}>13fasdfasdf</div>
+        <div style={{fontSize : 22}}>1asdfasdfasdfas4</div>
+        <div style={{fontSize : 22}}>15asdfasdf</div>
+        <div style={{fontSize : 42}}>asdfasdfasddfasdfasdf16</div>
+        <div style={{fontSize : 22}}>1fasdfasdfasd7</div>
+        <div style={{fontSize : 22}}>1fasdfasdf8</div>
+        <div style={{fontSize : 42}}>asdfasdf19</div>
+        <div style={{fontSize : 22}}>2asdfasdf0</div>
       </TagCloud>
      );
   }
