ccc7fb0d3b75dc601f806c9bbefedc0b3ead1b6c
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-11-04 22:13:28 +0900

    컴포넌트 분할

--- meant/src/container/index.js
+++ meant/src/container/index.js
@@ -4,6 +4,7 @@ import Register from './Member/Register';
 import CreateFunding from './CreateFunding/CreateFunding';
 import StartFunding from './StartFunding/StartFunding';
 import OrderNumber from './OrderNumber/OrderNumber';
+import Checked from './OrderNumber/Checked';
 
 export {
   Main,
@@ -11,6 +12,7 @@ export {
   Register,
   CreateFunding,
   StartFunding,
-  OrderNumber
+  OrderNumber,
+  Checked
   // 더 있으면 추가
 }
