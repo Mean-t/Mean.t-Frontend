3c16f19a10664492d927673b0e9b1985eab922d5
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-11-04 22:13:16 +0900

    Router 주석

--- meant/src/App.js
+++ meant/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { Main, Login, Register, CreateFunding, StartFunding, OrderNumber } from './container/index';
+import { Main, Login, Register, CreateFunding, StartFunding, OrderNumber, Checked } from './container/index';
 import NavigationBar from './components/default layout/NavigationBar';
 
 class App extends Component {
@@ -11,14 +12,27 @@ class App extends Component {
           <React.Fragment>
             <NavigationBar />
               <Switch>
-                <Route path='/' component={Main} exact/> {/* 메인 페이지 */}
-                <Route path='/member/login' component={Login} exact/> {/* 로그인 페이지 */}
-                <Route path='/member/join_check' component={Register} exact/> {/* 회원가입 페이지 */}
-                <Route path='/funding/create_idea' component={CreateFunding} exact/> {/* 아이디어 보내기 */}
-                <Route path='/funding/start' component={StartFunding} exact/> {/* 펀딩 시작하기 */}
-                <Route path='/order_number/reference' component={OrderNumber} exact/> {/* 주문번호 조회 */}
+
+                 {/* 메인 페이지 */}
+                <Route path='/' component={Main} exact/>
+
+                 {/* 로그인 페이지 */}
+                <Route path='/member/login' component={Login} exact/>
+
+                 {/* 회원가입 페이지 */}
+                <Route path='/member/join_check' component={Register} exact/>
+
+                 {/* 아이디어 보내기 */}
+                <Route path='/funding/create_idea' component={CreateFunding} exact/>
+
+                 {/* 펀딩 시작하기 */}
+                <Route path='/funding/start' component={StartFunding} exact/>
+
+                 {/* 주문번호 조회 */}
+                <Route path='/order_number/reference' component={Checked} exact/>
+                <Route path='/order_number/reference/:orderNumber' component={OrderNumber} exact/>
               </Switch>
-          </React.Fragment>
+            </React.Fragment>
         </BrowserRouter>
       </div>
     );
